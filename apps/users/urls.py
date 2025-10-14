14e84fb4318686b9d524be7234a64d039e2839cb
Kholmumin <xolmomin@gmail.com>
2025-10-14 09:20:44 +0500

    add task

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,12 +4,12 @@ from users.views import CourseTemplateView, InterviewListView, InterviewDetailVi
     BlogTemplateView, MainTemplateView, BlogTemplateDetailView, RegisterTemplateView, LoginTemplateView
 
 urlpatterns = [
+    path('', MainTemplateView.as_view(), name='main'),
     path('interviews', InterviewListView.as_view(), name='interview_list'),
     path('interview/<uuid:pk>', InterviewDetailView.as_view(), name='interview_detail'),
     path('courses', CourseTemplateView.as_view(), name='course_list'),
     path('blogs', BlogTemplateView.as_view(), name='blog_list'),
     path('blogs-detail', BlogTemplateDetailView.as_view(), name='blog_detail'),
-    path('', MainTemplateView.as_view(), name='main'),
     path('register', RegisterTemplateView.as_view(), name='register'),
     path('login', LoginTemplateView.as_view(), name='login'),
 ]
