00aec1d4839ac3f802e46111cbe4722f82a5aafe
mirjalol-07 <mirjalolzari0906@gmail.com>
2025-10-18 10:46:34 +0500

    add

---
+++ work.py
@@ -0,0 +1,454 @@
+import json
+
+data = [
+            {
+              "id": "52644e44-2b85-4524-a00a-19e9fc9950ec",
+              "title": "Backendga kirish",
+              "url": "backendga-kirish",
+              "description": "Backendga kirish",
+              "parts": [
+                {
+                  "id": "90c38fcf-98c6-480a-af1e-d3180cdf3d51",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "ENKy0xVK",
+                  "mediaUrl": "https://play.boomstream.com/ENKy0xVK",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "6cb26cf3-12ab-44b1-a41b-50d5837025e6",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "kbB30Msk",
+                  "mediaUrl": "https://play.boomstream.com/kbB30Msk",
+                  "title": "2-qism",
+                  "ord": 2
+                },
+                {
+                  "id": "6e1dff6d-8d85-4ef0-893d-7aaff60fd127",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "ItDDVnPX",
+                  "mediaUrl": "https://play.boomstream.com/ItDDVnPX",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 1
+            },
+            {
+              "id": "048e9418-53f3-4716-a941-045a5e0847da",
+              "title": "Template bilan ishlash, static fayllar, base.htmlga ajratish",
+              "url": "template-bilan-ishlash-static-fayllar-basehtmlga-ajratish",
+              "description": "Template bilan ishlash, static fayllar, base.htmlga ajratish",
+              "parts": [
+                {
+                  "id": "e668acc2-2bef-4f10-a2ce-c2ae6cfaa957",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "rdUJPoVI",
+                  "mediaUrl": "https://play.boomstream.com/rdUJPoVI",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "0fb3f5e9-3b33-422e-bf44-8716b4793433",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "j3REERPD",
+                  "mediaUrl": "https://play.boomstream.com/j3REERPD",
+                  "title": "2-qism",
+                  "ord": 2
+                },
+                {
+                  "id": "2bdc2f72-6030-41c1-8d45-9edab0ab1cc3",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "eXPxewCR",
+                  "mediaUrl": "https://play.boomstream.com/eXPxewCR",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 2
+            },
+            {
+              "id": "8a060d71-dae8-4866-a09f-fed5614e4d20",
+              "title": "Django admin page",
+              "url": "django-admin-page",
+              "description": "Django admin",
+              "parts": [
+                {
+                  "id": "eae2a2b3-6df0-4c32-aafb-4cf3763daefd",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "7SIeuceV",
+                  "mediaUrl": "https://play.boomstream.com/7SIeuceV",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "e0f6790e-d42f-4e00-8d45-38c6762927af",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "99Mrrk6I",
+                  "mediaUrl": "https://play.boomstream.com/99Mrrk6I",
+                  "title": "2-qism",
+                  "ord": 2
+                },
+                {
+                  "id": "ba21193f-e9f7-4f03-b930-87aa65b8e9dd",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "tv5DQDe0",
+                  "mediaUrl": "https://play.boomstream.com/tv5DQDe0",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 3
+            },
+            {
+              "id": "b16352bb-0b02-4b4f-a7c9-4365d9d6a37f",
+              "title": "Django ORM",
+              "url": "django-orm",
+              "description": "Django ORM",
+              "parts": [
+                {
+                  "id": "6d9da0e2-6b95-4961-ba9e-6cc5f03a0b87",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "EEUb7xFI",
+                  "mediaUrl": "https://play.boomstream.com/EEUb7xFI",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "3327b785-5318-420e-b805-0760cf25f94e",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "L5qXzezS",
+                  "mediaUrl": "https://play.boomstream.com/L5qXzezS",
+                  "title": "2-qism",
+                  "ord": 2
+                },
+                {
+                  "id": "16d5760a-5993-4b5d-a429-182b54d44700",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "A53N2eIW",
+                  "mediaUrl": "https://play.boomstream.com/A53N2eIW",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 4
+            },
+            {
+              "id": "85b3e9a4-3c1d-45c2-b0bf-896027e77728",
+              "title": "Django custom commands",
+              "url": "django-custom-commands",
+              "description": "Django custom commands",
+              "parts": [
+                {
+                  "id": "a87cc669-a363-4afd-b59a-33ec92b5604d",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "MIUKPFfF",
+                  "mediaUrl": "https://play.boomstream.com/MIUKPFfF",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "c5f0b0dd-402b-49a5-bb9c-6f502e44602a",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "ZJyOIf6W",
+                  "mediaUrl": "https://play.boomstream.com/ZJyOIf6W",
+                  "title": "2-qism",
+                  "ord": 2
+                },
+                {
+                  "id": "0838b118-e40f-4b05-9b68-dda47b928544",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "MbyxtLp7",
+                  "mediaUrl": "https://play.boomstream.com/MbyxtLp7",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 5
+            },
+            {
+              "id": "826b1137-c8b0-4f47-aad3-237e0c7c59a3",
+              "title": "Django 3rd party packages - import-export, django filters, mptt, django-baton, django-crispy-form",
+              "url": "django-3rd-party-packages---import-export-django-filters-mptt-django-baton-django-crispy-form",
+              "description": "Django 3rd party packages - import-export, django filters, mptt, django-baton, django-crispy-form",
+              "parts": [
+                {
+                  "id": "d1b1532d-42f7-46f4-a7d1-7b2411ceb96c",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "QH4DYJtr",
+                  "mediaUrl": "https://play.boomstream.com/QH4DYJtr",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "f7a24518-1967-4098-a23e-606fbf3286c7",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "Hwr6vq8V",
+                  "mediaUrl": "https://play.boomstream.com/Hwr6vq8V",
+                  "title": "2-qism",
+                  "ord": 2
+                },
+                {
+                  "id": "2231e6ae-3c5a-432f-8314-32606cd971c3",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "04griRLM",
+                  "mediaUrl": "https://play.boomstream.com/04griRLM",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 6
+            },
+            {
+              "id": "140f7668-ed1f-4372-b00d-d8fb4dc6064e",
+              "title": "Minio, ckeditor5",
+              "url": "minio-ckeditor5",
+              "description": "minio, ckeditor5",
+              "parts": [
+                {
+                  "id": "6344ecc5-68a9-4aa7-b1d0-7e90170a1202",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "VBTFztP0",
+                  "mediaUrl": "https://play.boomstream.com/VBTFztP0",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "0d49d0cb-c409-4c47-a71b-24a3c7088a97",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "WgFkW2W4",
+                  "mediaUrl": "https://play.boomstream.com/WgFkW2W4",
+                  "title": "2-qism",
+                  "ord": 2
+                },
+                {
+                  "id": "34b2606a-57ac-4e87-9a3e-9bf2a81ed7ff",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "EBvwAl4z",
+                  "mediaUrl": "https://play.boomstream.com/EBvwAl4z",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 7
+            },
+            {
+              "id": "d6a9e0d8-4899-4721-93d2-eec582442ec3",
+              "title": "Testing templated based",
+              "url": "testing-templated-based",
+              "description": "testing templated based",
+              "parts": [
+                {
+                  "id": "6e19704c-df72-4e11-90c9-83698703f33e",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "fHXC2fqX",
+                  "mediaUrl": "https://play.boomstream.com/fHXC2fqX",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "534f9570-40a6-4ef0-8d93-7ff54a74ade2",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "HIwA30Qn",
+                  "mediaUrl": "https://play.boomstream.com/HIwA30Qn",
+                  "title": "2-qism",
+                  "ord": 2
+                },
+                {
+                  "id": "2b54e37d-3ed2-4ff3-9ac3-1c94748e2567",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "AIKtjGcL",
+                  "mediaUrl": "https://play.boomstream.com/AIKtjGcL",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 8
+            },
+            {
+              "id": "97a01c18-63dc-4dfc-9510-281d1f361a38",
+              "title": "Celery tasklar bilan ishlash",
+              "url": "celery-tasklar-bilan-ishlash",
+              "description": "celery tasklar bilan ishlash",
+              "parts": [
+                {
+                  "id": "d4079a16-8758-4878-bfeb-90725629aa67",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "dsTtWpT5",
+                  "mediaUrl": "https://play.boomstream.com/dsTtWpT5",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "56af73e8-61b3-44fc-858c-494698ae5a52",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "ojjroV16",
+                  "mediaUrl": "https://play.boomstream.com/ojjroV16",
+                  "title": "2-qism",
+                  "ord": 2
+                },
+                {
+                  "id": "7ebf5aef-0102-4d4e-90a8-497a99052e4b",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "BdA5s3K6",
+                  "mediaUrl": "https://play.boomstream.com/BdA5s3K6",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 9
+            },
+            {
+              "id": "15ac47ee-e300-439e-93b9-836582d53bdc",
+              "title": "Celery-beat, celery-results",
+              "url": "celery-beat-celery-results",
+              "description": "celery-beat, celery-results",
+              "parts": [
+                {
+                  "id": "02980418-6101-41a1-bf89-b05db2ba7bd3",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "HeOWkmPd",
+                  "mediaUrl": "https://play.boomstream.com/HeOWkmPd",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "5b98fd8d-bff6-4ac9-a470-85112aede4a2",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "XuZGN3ek",
+                  "mediaUrl": "https://play.boomstream.com/XuZGN3ek",
+                  "title": "2-qism",
+                  "ord": 2
+                }
+              ],
+              "ord": 10
+            },
+            {
+              "id": "91dc5f43-ba90-45bf-b6ad-5f72bdcf1a3b",
+              "title": "Django publish own package",
+              "url": "django-publish-own-package",
+              "description": "django publish own package",
+              "parts": [
+                {
+                  "id": "92d84b5c-d0c6-4644-b719-6a79dd292ab4",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "FXa3Sgfd",
+                  "mediaUrl": "https://play.boomstream.com/FXa3Sgfd",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "c4ed0965-c102-49d3-afda-77a44180c058",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "vy9xz4OT",
+                  "mediaUrl": "https://play.boomstream.com/vy9xz4OT",
+                  "title": "2-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "6140babb-8b89-4091-b4a1-d94c6b19c50f",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "wss2osxu",
+                  "mediaUrl": "https://play.boomstream.com/wss2osxu",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 11
+            },
+            {
+              "id": "b46f57d3-49fc-4d3d-b5bf-e912b676af80",
+              "title": "Optimization with django-debug-toolbar",
+              "url": "optimization-with-django-debug-toolbar",
+              "description": "optimization with django-debug-toolbar",
+              "parts": [
+                {
+                  "id": "d9612b23-57a1-4118-8d21-e13309dbf2d5",
+                  "description": "1-qism",
+                  "isYoutube": False,
+                  "mediaCode": "8kGPA00E",
+                  "mediaUrl": "https://play.boomstream.com/8kGPA00E",
+                  "title": "1-qism",
+                  "ord": 1
+                },
+                {
+                  "id": "b5d608f8-9e7e-4850-b85b-e2cdb006bd6b",
+                  "description": "2-qism",
+                  "isYoutube": False,
+                  "mediaCode": "Dm88FX9i",
+                  "mediaUrl": "https://play.boomstream.com/Dm88FX9i",
+                  "title": "2-qism",
+                  "ord": 2
+                },
+                {
+                  "id": "19c89571-16b3-4fc7-9526-9063de1efa17",
+                  "description": "3-qism",
+                  "isYoutube": False,
+                  "mediaCode": "QaYJTR5v",
+                  "mediaUrl": "https://play.boomstream.com/QaYJTR5v",
+                  "title": "3-qism",
+                  "ord": 3
+                }
+              ],
+              "ord": 12
+            }
+          ]
+
+output = []
+
+for section in data:
+    section_id = section["id"]  # parent section ID
+
+    for part in section.get("parts", []):
+        lesson = {
+            "model": "users.lesson",
+            "pk": part["id"],
+            "fields": {
+                "updated_at": "2025-09-15T06:17:07.324Z",
+                "created_at": "2025-09-15T06:17:07.324Z",
+                "order_number": part.get("ord"),
+                "name": part.get("title"),
+                "status": "unpublished",
+                "access_type": "private",
+                "video_duration": 0,
+                "section": section_id,
+                "video_link": part.get("mediaUrl"),
+                "video": "videos/2025/09/15/example_video_vMb6u3o.mp4"
+            }
+        }
+        output.append(lesson)
+
+with open("lesson_fixture.json", "w", encoding="utf-8") as f:
+    json.dump(output, f, indent=2, ensure_ascii=False)
